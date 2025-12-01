c53a0528973a38a7901b449c2e7cbc7243cd6082
Muhammad Aimal Siddique <aimaldev5@gmail.com>
2025-12-01 21:52:49 +0500

    Add dashboard pages

---
+++ app/dashboard/seller/page.tsx
@@ -0,0 +1,8 @@
+
+export default function SellerDashboard() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h2 className="text-3xl font-bold text-white">Seller Dashbaord</h2>
+    </div>
+  );
+}
